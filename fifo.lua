ab2c24af4f0f5d33c23214c840462eb95b08236f
daurnimator <quae@daurnimator.com>
2015-03-18 17:18:55 -0400

    Store items in a .data field.
    This gives us much better performance in luajit.

--- fifo.lua
+++ fifo.lua
@@ -8,18 +8,19 @@ local fifo = {}
 local fifo_mt = {
 	__index = fifo ;
 	__newindex = function ( f , k , v )
-		if type (k) ~= "number" then
-			error ( "Tried to set value in fifo" )
-		else
-			return rawset ( f , k , v )
-		end
+		error("Tried to set table field in fifo")
 	end ;
 }
 
 local empty_default = function ( _ ) error ( "Fifo empty" ) end
 
 function fifo.new ( ... )
-	return setmetatable ( { empty = empty_default , head = 1 , tail = select("#",...) , ... } , fifo_mt )
+	return setmetatable({
+		empty = empty_default;
+		head = 1;
+		tail = select("#",...);
+		data = {...};
+	}, fifo_mt)
 end
 
 function fifo:length ( )
@@ -36,21 +37,21 @@ function fifo:peek ( n )
 	if index > self.tail then
 		return nil, false
 	else
-		return self [ index ], true
+		return self.data[index], true
 	end
 end
 
 function fifo:push ( v )
 	self.tail = self.tail + 1
-	self [ self.tail ] = v
+	self.data[self.tail] = v
 end
 
 function fifo:pop ( )
 	local head , tail = self.head , self.tail
 	if head > tail then return self:empty() end
 
-	local v = self [ head ]
-	self [ head ] = nil
+	local v = self.data[head]
+	self.data[head] = nil
 	self.head = head + 1
 	return v
 end
@@ -65,15 +66,15 @@ function fifo:insert ( n , v )
 	local p = head + n - 1
 	if p <= (head + tail)/2 then
 		for i = head , p do
-			self [ i - 1 ] = self [ i ]
+			self.data[i- 1] = self.data[i]
 		end
-		self [ p - 1 ] = v
+		self.data[p- 1] = v
 		self.head = head - 1
 	else
 		for i = tail , p , -1 do
-			self [ i + 1 ] = self [ i ]
+			self.data[i+ 1] = self.data[i]
 		end
-		self [ p ] = v
+		self.data[p] = v
 		self.tail = tail + 1
 	end
 end
@@ -88,16 +89,16 @@ function fifo:remove ( n )
 	if head + n - 1 > tail then return self:empty() end
 
 	local p = head + n - 1
-	local v = self [ p ]
+	local v = self.data[p]
 
 	if p <= (head + tail)/2 then
 		for i = p , head , -1 do
-			self [ i ] = self [ i - 1 ]
+			self.data[i] = self.data[i-1]
 		end
 		self.head = head + 1
 	else
 		for i = p , tail do
-			self [ i ] = self [ i + 1 ]
+			self.data[i] = self.data[i+1]
 		end
 		self.tail = tail - 1
 	end
